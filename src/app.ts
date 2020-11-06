0091259ec438993b80b137f6919ba4e29a6e25e9
Ryukyo <florian.fejer@hotmail.de>
2020-11-06 14:18:29 +0100

    Render form

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,20 @@
-// Code goes here!
+class ProjectInput {
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    formElement: HTMLFormElement;
+
+    constructor() {
+        this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
+        this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+        const importedContent = document.importNode(this.templateElement.content, true);
+        this.formElement = importedContent.firstElementChild as HTMLFormElement;
+        this.attachHTML();
+    }
+
+    private attachHTML () {
+        this.hostElement.insertAdjacentElement('afterbegin', this.formElement)
+    }
+}
+
+const newInput = new ProjectInput();
